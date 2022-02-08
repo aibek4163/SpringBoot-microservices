32fb1c6365f806f5ad6bd153bda8513b1f176317
Aibek <nurlanov.aibek1@gmail.com>
2022-02-08 12:16:27 +0600

    Configured POM

---
+++ notification/src/main/java/com/microservices/notification/NotificationApplication.java
@@ -0,0 +1,31 @@
+package com.microservices.notification;
+
+import com.microservices.amqp.RabbitMQMessageProducer;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.microservices.notification",
+                "com.microservices.amqp",
+        }
+)
+public class NotificationApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationApplication.class, args);
+    }
+
+//    @Bean
+//    CommandLineRunner commandLineRunner(RabbitMQMessageProducer producer, NotificationConfig notificationConfig) {
+//        return args -> {
+//            producer.publish(new Person("Test", 16),
+//                    notificationConfig.getInternalExchange(),
+//                    notificationConfig.getInternalNotificationRoutingKey());
+//        };
+//    }
+//
+//    record Person(String name, int age) {
+//    }
+}
